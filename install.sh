8e8da9f63bf3060e97310f6cc16030c34b28d76e
huyle <huy@huy.pop>
2021-05-10 09:54:14 +0700

    tmux

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-ln -s $HOME/.config/dotfiles/tmux /$HOME/.tmux
-ln -s $HOME/.config/dotfiles/tmux/.tmux
+ln -s $HOME/.config/dotfiles/.tmux /$HOME/.tmux
+ln -s $HOME/.config/dotfiles/.tmux/.tmux.conf /$HOME/.tmux.conf
+ln -s $HOME/.config/dotfiles/.tmux/.tmux.conf.local /$HOME/.tmux.conf.local
